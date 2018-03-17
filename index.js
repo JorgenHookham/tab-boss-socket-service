2426def58b6b884d2e2a99b2c3edce889211c1d3
Jorgen Hookham <Jorgen@Hookham.co>
2018-03-17 00:07:05 -0700

    remove junk and update readme

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const wss = new WebSocket.Server({port: 8080});
 const stdin = process.openStdin();
 
 wss.broadcast = function broadcast (data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-    });
+	wss.clients.forEach(function each(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
 }
 
 stdin.addListener("data", function(d) {
-    let input = d.toString().trim();
-    wss.broadcast(input);
-    console.log(`You said "${input}"`);
+	let input = d.toString().trim();
+	wss.broadcast(input);
+	console.log(`You said "${input}"`);
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tab Boss Sockets Service
+
+A simple web sockets service to integrate with the [Tab Boss chrome extension](git clone https://github.com/JorgenHookham/tab-boss.git).
