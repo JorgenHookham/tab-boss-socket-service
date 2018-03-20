6649133ac9cc951cfd3bc6c6cc6c582643bca6ab
Jorgen Hookham <Jorgen@Hookham.co>
2018-03-19 21:13:49 -0700

    whoops

--- server.js
+++ server.js
@@ -19,9 +19,12 @@ const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
 	console.log('Client connected');
-	ws.on('close', () => console.log('Client disconnected'));
 
-	wss.on('message', (data) => {
+	ws.on('close', () => {
+		console.log('Client disconnected')
+	});
+
+	ws.on('message', (data) => {
 		console.log(data);
 	});
 });
